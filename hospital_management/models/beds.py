9e1354491982eaf7740056d937911c147af25bdd
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-18 17:29:17 +0600

    create menu

--- hospital_management/models/beds.py
+++ hospital_management/models/beds.py
@@ -1,6 +1,18 @@
 from odoo import api, fields, models
 
+
 class HospitalBed(models.Model):
     _name = 'hospital.bed'
     _description = 'Hospital Bed Information'
 
+    bed_no = fields.Integer(string='Bed No', required=True)
+    bed_type = fields.Selection([('gatch', 'Gatch Bed'),
+                                 ('electric', 'Electric'),
+                                 ('stretcher', 'Stretcher'),
+                                 ('low', 'Low Bed'),
+                                 ('air', 'Low Air Loss'),
+                                 ('circo', 'Circo Electric'),
+                                 ('clinitron', 'Clinitron'),
+                                 ], string='Bed Type')
+    note = fields.Text(string='Notes')
+    date_bed_assign = fields.Date(default=fields.Date.today, string='Date')

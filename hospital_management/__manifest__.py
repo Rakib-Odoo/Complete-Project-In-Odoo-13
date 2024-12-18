49f797dae6aea39651c72726be02d90e0a2451f6
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-18 17:17:01 +0600

    first module create

---
+++ hospital_management/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Hospital Management',
+    'description': """
+        Hospital management module which is used to mange the hospital functionalities prescription,patient,doctor diagnosis etc
+    """,
+    'summary': """
+    Hospital management module which is used to mange the hospital functionalities prescription,patient,doctor diagnosis etc
+""",
+    'author': "Fahim Hasan Rakib",
+    "license": "AGPL-3",
+    'category': 'Hospital',
+    'version': '13.0.1.0.2',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ hospital_management/models/beds.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models
+
+class HospitalBed(models.Model):
+    _name = 'hospital.bed'
+    _description = 'Hospital Bed Information'
+
